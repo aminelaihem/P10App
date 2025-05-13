f77bda9fc4dcf77ad207d87a94ea5b14d5f3c884
aminelhm <131161302+aminelhm@users.noreply.github.com>
2025-05-13 09:38:35 +0200

    front auth (base)

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&family=Barlow+Condensed:wght@600&display=swap" rel="stylesheet" />
       <body className="antialiased">
         <Main />
         <NextScript />
